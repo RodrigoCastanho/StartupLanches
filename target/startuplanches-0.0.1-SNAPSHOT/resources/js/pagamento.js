ae233ac87feb8b466e79d5e09fb9c2955d2531f2
Rodrigo Castanho <rodrigo.fcastanho@gmail.com>
2019-05-25 02:33:03 -0300

    Sistema Web para startup teste

---
+++ target/startuplanches-0.0.1-SNAPSHOT/resources/js/pagamento.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+ $('#concluirpedido').click(function(){
+  
+     alert(           "\tBom Apetite"+
+     		"\n\n\nDEBITO        CREDITO");
+     
+  
+  });
+});
+
+
+
